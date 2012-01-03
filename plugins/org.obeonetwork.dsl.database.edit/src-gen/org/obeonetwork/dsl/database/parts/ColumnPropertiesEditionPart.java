e1a9224b145852bf5919f0593dfc61098b0b342a
Stephane Thibaudeau <stephane.thibaudeau@obeo.fr>
2012-01-03 17:27:55 +0100

    Specific EEF properties view for Column

--- plugins/org.obeonetwork.dsl.database.edit/src-gen/org/obeonetwork/dsl/database/parts/ColumnPropertiesEditionPart.java
+++ plugins/org.obeonetwork.dsl.database.edit/src-gen/org/obeonetwork/dsl/database/parts/ColumnPropertiesEditionPart.java
@@ -4,6 +4,7 @@
 package org.obeonetwork.dsl.database.parts;
 
 // Start of user code for imports
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.eef.runtime.ui.widgets.ButtonsModeEnum;
 import org.eclipse.emf.eef.runtime.ui.widgets.eobjflatcombo.EObjectFlatComboSettings;
@@ -31,6 +32,91 @@ public interface ColumnPropertiesEditionPart {
 	 * 
 	 */
 	public void setName(String newValue);
+
+
+	/**
+	 * @return the type
+	 * 
+	 */
+	public Object getType();
+
+	/**
+	 * Init the type
+	 * @param input choice of values
+	 * @param currentValue the current value
+	 */
+	public void initType(Object input, Object currentValue);
+
+	/**
+	 * Defines a new type
+	 * @param newValue the new type to set
+	 * 
+	 */
+	public void setType(Object newValue);
+
+	/**
+	 * Adds the given filter to the type edition editor.
+	 * 
+	 * @param filter
+	 *            a viewer filter
+	 * @see org.eclipse.jface.viewers.StructuredViewer#addFilter(ViewerFilter)
+	 * 
+	 */
+	public void addFilterToType(ViewerFilter filter);
+
+
+	/**
+	 * @return the length
+	 * 
+	 */
+	public String getLength();
+
+	/**
+	 * Defines a new length
+	 * @param newValue the new length to set
+	 * 
+	 */
+	public void setLength(String newValue);
+
+
+	/**
+	 * @return the precision
+	 * 
+	 */
+	public String getPrecision();
+
+	/**
+	 * Defines a new precision
+	 * @param newValue the new precision to set
+	 * 
+	 */
+	public void setPrecision(String newValue);
+
+
+	/**
+	 * @return the literals
+	 * 
+	 */
+	public EList getLiterals();
+
+	/**
+	 * Defines a new literals
+	 * @param newValue the new literals to set
+	 * 
+	 */
+	public void setLiterals(EList newValue);
+
+	/**
+	 * Add a value to the literals multivalued attribute.
+	 * @param newValue the value to add
+	 */
+	public void addToLiterals(Object newValue);
+
+	/**
+	 * Remove a value to the literals multivalued attribute.
+	 * @param newValue the value to remove
+	 */
+	public void removeToLiterals(Object newValue);
 
 
 	/**
@@ -176,7 +262,7 @@ public interface ColumnPropertiesEditionPart {
 	public String getTitle();
 
 	// Start of user code for additional methods
-
+	public void updateTypeFields(boolean lengthVisible, boolean precisionVisible, boolean literalsVisible);
 // End of user code
 
 }
